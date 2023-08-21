897bd4bcdec73f1460e36d0f4b422eb5a169f711
Yhay Nesb <yhya.syrian@gmail.com>
2023-08-21 10:23:22 +0300

    start

---
+++ README.md
@@ -0,0 +1,9 @@
+# e-commerce
+for install
+```
+git clone https://github.com/yhyasyrian/e-commerce.git
+cd e-commerce.git
+composer i
+npm i
+npm run build
+```
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./resources/**/*.blade.php",
+    "./resources/**/*.ts",
+    "./resources/**/*.js",
+    "./resources/**/*.css",
+    "./app/Http/Controllers/**/*.php"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
