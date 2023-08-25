d65d4436f503b8e9feaec4ac6d4ed626d560dbc4
fadel-hasan <80330523+fadel-hasan@users.noreply.github.com>
2023-08-25 16:07:44 +0300

    add_dev_tool_debugbar

--- config/app.php
+++ config/app.php
@@ -163,6 +163,7 @@ return [
         /*
          * Application Service Providers...
          */
+        Barryvdh\Debugbar\ServiceProvider::class,
         App\Providers\AppServiceProvider::class,
         App\Providers\AuthServiceProvider::class,
         // App\Providers\BroadcastServiceProvider::class,
@@ -182,6 +183,7 @@ return [
     */
 
     'aliases' => Facade::defaultAliases()->merge([
+        'Debugbar' => Barryvdh\Debugbar\Facades\Debugbar::class,
         // 'Example' => App\Facades\Example::class,
     ])->toArray(),
 
